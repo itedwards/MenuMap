11e3ce1ffde4cdae5f9f0d311b2c0c417af5c759
Matthew Barnard <mattyice@live.unc.edu>
2019-11-22 11:04:27 -0500

    Layout changes for UI

--- app/src/main/java/com/example/menumap/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/menumap/ui/dashboard/DashboardFragment.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -48,6 +49,8 @@ public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
     private ImageView mImageView;
+    private Button mCameraBtn;
+    private TextView mResult;
     private FirebaseVisionTextRecognizer mDetector;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -55,7 +58,11 @@ public class DashboardFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         mImageView = root.findViewById(R.id.imageView);
-        mImageView.setOnClickListener(new View.OnClickListener() {
+        mImageView.setVisibility(View.GONE);
+        mResult = root.findViewById(R.id.resultView);
+        mResult.setVisibility(TextView.GONE);
+        mCameraBtn = root.findViewById(R.id.cameraBtn);
+        mCameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -76,10 +83,7 @@ public class DashboardFragment extends Fragment {
 
     private FirebaseVisionImage imageFromBitmap(Bitmap bitmap) {
         // [START image_from_bitmap]
-        Image image = (Image) bitmap;
-        int rotation = getRotationCompensation(
-                CameraCharacteristics.LENS_FACING_FRONT, MainActivity.this, this);
-        return FirebaseVisionImage.fromMediaImage(image, rotation);
+        return FirebaseVisionImage.fromBitmap(bitmap);
         // [END image_from_bitmap]
     }
 
@@ -91,7 +95,8 @@ public class DashboardFragment extends Fragment {
             if(extras != null) {
                 Bitmap bm = (Bitmap) extras.get("data");
                 this.mImageView.setImageBitmap(bm);
-
+                mImageView.setVisibility(View.VISIBLE);
+                mCameraBtn.setVisibility(Button.GONE);
                 FirebaseVisionImage image = imageFromBitmap(bm);
 
 
@@ -99,7 +104,9 @@ public class DashboardFragment extends Fragment {
                         .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                             @Override
                             public void onSuccess(FirebaseVisionText result) {
+                                mResult.setVisibility(View.VISIBLE);
                                 String resultText = result.getText();
+                                mResult.setText(result.getText());
                                 Log.d("result", resultText);
                             }
                         })
@@ -113,57 +120,7 @@ public class DashboardFragment extends Fragment {
             }
         }
     }
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
-    }
 
-        /**
-         * Get the angle by which an image must be rotated given the device's current
-         * orientation.
-         */
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        private int getRotationCompensation(String cameraId, Activity activity, Context context)
-                throws CameraAccessException {
-            // Get the device's current rotation relative to its "native" orientation.
-            // Then, from the ORIENTATIONS table, look up the angle the image must be
-            // rotated to compensate for the device's rotation.
-            int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-            int rotationCompensation = ORIENTATIONS.get(deviceRotation);
-
-            // On most devices, the sensor orientation is 90 degrees, but for some
-            // devices it is 270 degrees. For devices with a sensor orientation of
-            // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
-            CameraManager cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
-            int sensorOrientation = cameraManager
-                    .getCameraCharacteristics(cameraId)
-                    .get(CameraCharacteristics.SENSOR_ORIENTATION);
-            rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
-
-            // Return the corresponding FirebaseVisionImageMetadata rotation value.
-            int result;
-            switch (rotationCompensation) {
-                case 0:
-                    result = FirebaseVisionImageMetadata.ROTATION_0;
-                    break;
-                case 90:
-                    result = FirebaseVisionImageMetadata.ROTATION_90;
-                    break;
-                case 180:
-                    result = FirebaseVisionImageMetadata.ROTATION_180;
-                    break;
-                case 270:
-                    result = FirebaseVisionImageMetadata.ROTATION_270;
-                    break;
-                default:
-                    result = FirebaseVisionImageMetadata.ROTATION_0;
-                    Log.e("Rotation", "Bad rotation value: " + rotationCompensation);
-            }
-            return result;
-        }
 
 
 }
